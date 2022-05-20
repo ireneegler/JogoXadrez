2801df5c6ddf72c50318b0bdcfb58a939664e72c
Irene Egler <irene.egler@gmail.com>
2022-05-20 10:49:42 -0300

    Comecando a implementar metodo que impprime na tela

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Tabuleiro
+namespace tabuleiro
 {
-    internal class Tabuleiro
+     class Tabuleiro
     {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pecas = new Peca[linhas, colunas];
+        }
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
     }
 }
